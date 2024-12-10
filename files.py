e504984e24652f4b0a10031d6fe1aa393c4252b3
David <davidmontesvilanova@gmail.com>
2024-12-10 13:20:24 -0800

    Clean up code | Created class and objects to house custom folders

---
+++ files.py
@@ -0,0 +1,39 @@
+import os, pathlib
+
+class Custom_Folder:
+
+    def __init__(self, dedicated_path : str, folder_name : str, file_extensions : list) -> None:
+        self.main_path = dedicated_path
+        self.folder_name =  "\\" + folder_name #TODO: ensure that "folder name" doesn't have a backslash character in the first character of the string
+        self.file_exts = file_extensions
+        self.folder_path = self.main_path + self.folder_name # The reason I am making these into seperate values (dedicated path and folder name) is just incase I was more customizability between values
+
+        if not os.path.exists(self.folder_path):
+            os.makedirs(self.folder_path)
+
+    def change_folder_name(self) -> None:
+        while True:
+            new_name = str(input("What would you like to rename your folder to?\n"))
+            if (new_name[0] != '\\'): #formatting
+                new_name = "\\" + new_name
+            if (os.path.exists(self.main_path + new_name) and new_name != self.folder_name): # check for discrepensies
+                choice = str(input(f"WARNING: path '{self.main_path + new_name}' already exists! Writing into this folder could cause unwanted actions. Are you sure you want to use this folder? (Y/N)\n"))
+                if (choice.upper() == "Y"):
+                    break
+            else:
+                choice = str(input(f"Are you satisfied with the name '{new_name[1:]}'? (Y/N)\n"))
+                if (choice.upper == 'Y'):
+                    break
+        self.transfer_files(self, new_name)
+        os.removedirs(self.folder_path)
+        self.folder_name = new_name
+        self.folder_path = self.main_path + self.folder_name
+        
+    def transfer_files(self, new_folder) -> None:
+        if not (os.path.exists(self.main_path + new_folder)):
+            os.makedirs(self.main_path + new_folder)
+        
+        for item in list(pathlib.Path(self.main_path).iterdir()):
+            str(item).replace(self.folder_path, '') #get the item alone
+            os.rename(self.folder_path + item, (self.main_path + new_folder) + item)
+    
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pathlib, os
+import files
+
+
 
 dedicated_path = input("Type the path that you want to clean: \n").replace('"','') # copying path on windows includes quotation marks, so I am removing them
 
@@ -6,33 +9,22 @@ image_extensions = [".jpg", ".png", "avif", "jpeg" ]
 
 m360_extensions = ["docx", "pptx", ".csv"]
 
-if not (os.path.exists(dedicated_path + r"\images")): # Creating folders to sort items into. Gonna make this into classes and stuff probably for better customizability
-    os.makedirs(dedicated_path + r"\images")
+images = files.Custom_Folder(dedicated_path, r"\images", image_extensions)
 
-if not (os.path.exists(dedicated_path + r"\pdfs")):
-    os.makedirs(dedicated_path + r"\pdfs")
-    
-if not (os.path.exists(dedicated_path + r"\360")):
-    os.makedirs(dedicated_path + r"\360")
+pdfs = files.Custom_Folder(dedicated_path, r"\pdfs", ".pdf")
 
-if not (os.path.exists(dedicated_path + r"\executables")):
-    os.makedirs(dedicated_path + r"\executables")
+m360 = files.Custom_Folder(dedicated_path, r"\360", m360_extensions)
+
+executable = files.Custom_Folder(dedicated_path, r"\executables", ".exe")
+
+custom_folders = [images, pdfs, m360, executable]
 
 for item in list(pathlib.Path(dedicated_path).iterdir()):
     print(f"{str(item)[-4:]}")
-    if (str(item)[-4:] in image_extensions):
-        #print("FILE FOUUUNDDDDDDDDDDDD\n")
-        item = str(item).replace(dedicated_path, '')
-        #print(folder_path + item)
-        os.rename(dedicated_path + item, (dedicated_path + r"\images") + item)
-    elif (str(item)[-4:] == ".pdf"):
-        item = str(item).replace(dedicated_path, '')
-        os.rename(dedicated_path + item, (dedicated_path + r"\pdfs") + item)
-    elif (str(item)[-4:] in m360_extensions):
-        item = str(item).replace(dedicated_path, '')
-        os.rename(dedicated_path + item, (dedicated_path + r"\360") + item)
-    elif (str(item)[-4:] == ".exe"):
-        item = str(item).replace(dedicated_path, '')
-        os.rename(dedicated_path + item, (dedicated_path + r"\executables") + item)
+    item = str(item).replace(dedicated_path, '')
+    for folder in custom_folders:
+        if item[-4:] in folder.file_exts:
+            os.rename(dedicated_path + item, folder.folder_path + item)
+
 
         
